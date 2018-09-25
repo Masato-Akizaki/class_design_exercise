42d5f9c3829300a7e92d0077acc7a6eb14b4ca45
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-09-25 18:06:52 +0900

    step1 unusable money

--- money.rb
+++ money.rb
@@ -6,4 +6,16 @@ class Money
     self.name = name
     self.value = value
   end
+
+  def charge_now
+    return "投入したお金: #{self.name}"
+  end
+
+  def unusable_money(total)
+    change = self.value
+    puts "#{self.name}は使用できません"
+    puts "現在の投入金額の合計:#{total}円"
+    return "釣り銭:#{change}円"
+  end
+
 end
