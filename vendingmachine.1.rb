e67eed853dea77cd786a8f84e05bf57faa75984d
Masato AKIZAKI <m.graceful_9411@icloud.com>
2018-10-04 10:46:53 +0900

    rewrite cord

--- vendingmachine.1.rb
+++ vendingmachine.1.rb
@@ -12,6 +12,11 @@ class VendingMachine
     5.times { store Drink.cola } 
   end
 
+  # @drink_table={"cola" => {:price=>120, :stock=>5}, 
+  #               "redbul" => {:price=>200, :stock=>1}
+  #               "water" => {:price=>100, :stock=>1 }
+
+
   def insert(money)  # お金の投入
     AVAILABLE_MONEY.include?(money) ? nil.tap { @total += money } : money 
   end 
@@ -22,21 +27,10 @@ class VendingMachine
 
   def store(drink)  # 指定したドリンクを格納する
     nil.tap do  
-      unless @drink_table.has_key? drink.name 
-        @drink_table[drink.name] =  { price: drink.price, stock: 0 }
-      end
+      @drink_table[drink.name] =  { price: drink.price, stock: 0 } unless @drink_table.has_key? drink.name 
       @drink_table[drink.name][:stock] += 1  
     end
   end
-
-  #def purchase(drink)  # 指定したドリンクを購入する
-  #  if purchasable? drink.name
-  #    @drink_table[drink.name][:stock] -= 1
-  #    @sale_amount += drink.price 
-  #    @total -= drink.price  
-  #    [drink.name, drink.price, refund]
-  #  end
-  #end
 
   def purchase(drink_name)  # 指定したドリンクを購入する
     if purchasable? drink_name
@@ -44,7 +38,7 @@ class VendingMachine
       price = @drink_table[drink_name][:price]
       @sale_amount += price 
       @total -= price  
-      [drink_name, price, refund]  
+      [drink_name, -price, refund]  
     end
   end
 
