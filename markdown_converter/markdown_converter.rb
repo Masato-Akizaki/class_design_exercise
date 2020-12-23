66184b0da5c4911e489ba48c89b8e911c61a09dd
Masato AKIZAKI <m.graceful_9411@icloud.com>
2020-12-24 06:33:10 +0900

    solve theme 2 to 4

--- markdown_converter/markdown_converter.rb
+++ markdown_converter/markdown_converter.rb
@@ -1,7 +1,22 @@
 class Markdown_Converter
 
-  def convert_p_element(text)
-    output = "<p>" + text + "</p>"
+  def convert(text)
+    array = text.split(" ", 2)
+    if array[0] == '#'
+      output = "<h1>" + array[1] + "</h1>"
+    elsif array[0] == '##'
+      output = "<h2>" + array[1] + "</h2>"
+    elsif array[0] == '###'
+      output = "<h3>" + array[1] + "</h3>"
+    elsif array[0] == '####'
+      output = "<h4>" + array[1] + "</h4>"
+    elsif array[0] == '#####'
+      output = "<h5>" + array[1] + "</h5>"
+    elsif array[0] == '######'
+      output = "<h6>" + array[1] + "</h6>"
+    else
+      output = "<p>" + text + "</p>"
+    end
   end
 
 end
