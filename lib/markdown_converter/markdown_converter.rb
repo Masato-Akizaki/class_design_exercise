4be48bd14da03750d0465c74848c1f4ddbf4ead1
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-02-06 19:22:06 +0900

    edit convert method

--- lib/markdown_converter/markdown_converter.rb
+++ lib/markdown_converter/markdown_converter.rb
@@ -3,23 +3,16 @@ class Markdown_Converter
   def convert_header(text)
     if text.start_with? "#"
       hashes, text = text.split(" ", 2)
-      "<h#{hashes.length}>#{text}</h#{hashes.length}>"
+      "<h#{hashes.length}>#{convert_strong(text)}</h#{hashes.length}>"
+    elsif text.start_with? "-"
+      _, text = text.split(" ", 2)
+      "<li>#{convert_strong(text)}</li>"
     else
-      "<p>" + text + "</p>"
+      "<p>" + convert_strong(text) + "</p>"
     end
   end
 
   def convert_strong(text)
     text.gsub("**").each_with_index { |_, i| "<#{"/" if i.odd?}strong>" }
   end
-
-  def convert_list(text)
-    if text.start_with? "-"
-      _, text = text.split(" ", 2)
-      "<li>#{text}</li>"
-    else
-      "<p>" + text + "</p>"
-    end
-  end
-
 end
