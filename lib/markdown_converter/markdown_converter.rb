c3751bebf6b87570875439183d649e01fec1b27b
Masato AKIZAKI <m.graceful_9411@icloud.com>
2021-02-07 19:58:37 +0900

    solve theme 7

--- lib/markdown_converter/markdown_converter.rb
+++ lib/markdown_converter/markdown_converter.rb
@@ -1,18 +1,22 @@
 class Markdown_Converter
 
-  def convert_header(text)
+  def convert_markdown(markdown)
+    markdown.split("\n").map { |line| convert_line(line) }.join("\n")
+  end
+
+  def convert_line(text)
     if text.start_with? "#"
       hashes, text = text.split(" ", 2)
-      "<h#{hashes.length}>#{convert_strong(text)}</h#{hashes.length}>"
+      "<h#{hashes.length}>#{convert_text(text)}</h#{hashes.length}>"
     elsif text.start_with? "-"
       _, text = text.split(" ", 2)
-      "<li>#{convert_strong(text)}</li>"
+      "<li>#{convert_text(text)}</li>"
     else
-      "<p>" + convert_strong(text) + "</p>"
+      "<p>" + convert_text(text) + "</p>"
     end
   end
 
-  def convert_strong(text)
+  def convert_text(text)
     text.gsub("**").each_with_index { |_, i| "<#{"/" if i.odd?}strong>" }
   end
 end
